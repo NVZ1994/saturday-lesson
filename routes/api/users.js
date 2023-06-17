ea8d3e325c95424e886ec84d03ec940a4b008bda
NVZ1994 <nikita.zhuk.1994@gmail.com>
2023-06-17 13:03:10 +0300

    done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login, logout } = require("../../controllers/usersController");
+
 const { signup, login, logout } = require("../../controllers/usersController");
 const userSignUpValidation = require("../../middlewares/validation/userSignUp");
 const userLoginValidation = require("../../middlewares/validation/userLogin");
+const authenticate = require("../../middlewares/authentificate");
+
 router.post("/signup", userSignUpValidation, signup);
 router.post("/login", userLoginValidation, login);
 router.post("/logout", authenticate, logout);
